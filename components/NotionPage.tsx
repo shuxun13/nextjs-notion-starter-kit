855a01bf43ca24450a9dae8ed63cb7067c56e5dc
shuxun13 <59434520+shuxun13@users.noreply.github.com>
2022-03-04 20:10:16 +0800

    Update NotionPage.tsx

--- components/NotionPage.tsx
+++ components/NotionPage.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import Head from 'next/head'
-import Link from 'next/link'
+//import Link from 'next/link'
 import dynamic from 'next/dynamic'
 import cs from 'classnames'
 import { useRouter } from 'next/router'
