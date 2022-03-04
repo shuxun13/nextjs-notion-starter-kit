e61fd004b7b62ca72131aecc0b8b93ee3f32519f
shuxun13 <59434520+shuxun13@users.noreply.github.com>
2022-03-04 14:19:07 +0800

    Update GitHubShareButton.tsx

--- components/GitHubShareButton.tsx
+++ components/GitHubShareButton.tsx
@@ -5,7 +5,7 @@ import styles from './styles.module.css'
 export const GitHubShareButton: React.FC = () => {
   return (
     <a
-      href='https://www.baidu.com'
+      href='https://www.baidu.com/'
       target='_blank'
       rel='noopener noreferrer'
       className={styles.githubCorner}
