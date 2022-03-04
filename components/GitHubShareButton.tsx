34c953fd8abd340c2d239771c0cc98d1f4a510be
shuxun13 <59434520+shuxun13@users.noreply.github.com>
2022-03-04 15:06:26 +0800

    Update GitHubShareButton.tsx

--- components/GitHubShareButton.tsx
+++ components/GitHubShareButton.tsx
@@ -12,8 +12,8 @@ export const GitHubShareButton: React.FC = () => {
       aria-label='View source on GitHub'
     >
       <svg
-        width='80'
-        height='80'
+        width='0'
+        height='0'
         viewBox='0 0 250 250'
         style={{
           fill: '#70B7FD',
@@ -32,7 +32,7 @@ export const GitHubShareButton: React.FC = () => {
           d='M128.3,109.0 C113.8,99.7 119.0,89.6 119.0,89.6 C122.0,82.7 120.5,78.6 120.5,78.6 C119.2,72.0 123.4,76.3 123.4,76.3 C127.3,80.9 125.5,87.3 125.5,87.3 C122.9,97.6 130.6,101.9 134.4,103.2'
           fill='currentColor'
           style={{
-            transformOrigin: '130px 106px'
+            transformOrigin: '0px 0px'
           }}
           className={styles.octoArm}
         />
