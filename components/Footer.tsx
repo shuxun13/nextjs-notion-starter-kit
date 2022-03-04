5f80551a16fe17155a162155eca0e192e8c1ab39
shuxun13 <59434520+shuxun13@users.noreply.github.com>
2022-03-04 16:35:49 +0800

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -25,58 +25,58 @@ export const Footer: React.FC<{
   }, [])
 
   return (
-    <footer className={styles.footer}>
-      <div className={styles.copyright}>Copyright 2021 {config.author}</div>
+    // <footer className={styles.footer}>
+    //   <div className={styles.copyright}>Copyright 2021 {config.author}</div>
 
-      {hasMounted ? (
-        <div className={styles.settings}>
-          <a
-            className={styles.toggleDarkMode}
-            onClick={toggleDarkModeCb}
-            title='Toggle dark mode'
-          >
-            {isDarkMode ? <IoMoonSharp /> : <IoSunnyOutline />}
-          </a>
-        </div>
-      ) : null}
+    //   {hasMounted ? (
+    //     <div className={styles.settings}>
+    //       <a
+    //         className={styles.toggleDarkMode}
+    //         onClick={toggleDarkModeCb}
+    //         title='Toggle dark mode'
+    //       >
+    //         {isDarkMode ? <IoMoonSharp /> : <IoSunnyOutline />}
+    //       </a>
+    //     </div>
+    //   ) : null}
 
-      <div className={styles.social}>
-        {config.twitter && (
-          <a
-            className={styles.twitter}
-            href={`https://twitter.com/${config.twitter}`}
-            title={`Twitter @${config.twitter}`}
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <FaTwitter />
-          </a>
-        )}
+    //   <div className={styles.social}>
+    //     {config.twitter && (
+    //       <a
+    //         className={styles.twitter}
+    //         href={`https://twitter.com/${config.twitter}`}
+    //         title={`Twitter @${config.twitter}`}
+    //         target='_blank'
+    //         rel='noopener noreferrer'
+    //       >
+    //         <FaTwitter />
+    //       </a>
+    //     )}
 
-        {config.github && (
-          <a
-            className={styles.github}
-            href={`https://github.com/${config.github}`}
-            title={`GitHub @${config.github}`}
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <FaGithub />
-          </a>
-        )}
+    //     {config.github && (
+    //       <a
+    //         className={styles.github}
+    //         href={`https://github.com/${config.github}`}
+    //         title={`GitHub @${config.github}`}
+    //         target='_blank'
+    //         rel='noopener noreferrer'
+    //       >
+    //         <FaGithub />
+    //       </a>
+    //     )}
 
-        {config.linkedin && (
-          <a
-            className={styles.linkedin}
-            href={`https://www.linkedin.com/in/${config.linkedin}`}
-            title={`LinkedIn ${config.author}`}
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <FaLinkedin />
-          </a>
-        )}
-      </div>
-    </footer>
+    //     {config.linkedin && (
+    //       <a
+    //         className={styles.linkedin}
+    //         href={`https://www.linkedin.com/in/${config.linkedin}`}
+    //         title={`LinkedIn ${config.author}`}
+    //         target='_blank'
+    //         rel='noopener noreferrer'
+    //       >
+    //         <FaLinkedin />
+    //       </a>
+    //     )}
+    //   </div>
+    // </footer>
   )
 }
